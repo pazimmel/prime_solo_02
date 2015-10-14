43e852086731cf302800831c3bf6cce030451157
Paul Zimmel <pazimmel@gmail.com>
2015-10-14 16:43:04 -0500

    Initial Commit

---
+++ solo_script.js
@@ -0,0 +1,92 @@
+//possible errors
+    //line 19 changed array[i] = calculateSTI(array) to array[i] = calculateSTI(array[i]). That way the input into the function calculateSTI is only the ith nested array in array.
+    // line 73, why is base percent - 1, I got rid of the -1, I may have to multiply it by 100
+    // line 47, the salary should be rounded
+// ! ! !
+// Three Bugs
+
+var arrayAtticus = ["Atticus", "2405", "47000", 3];
+var arrayJem = ["Jem", "62347", "63500", 4];
+var arrayBoo = ["Boo", "11435", "54000", 3];
+var arrayScout = ["Scout", "6243", "74750", 5]; //declare variables
+
+var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+
+//Create variables used to write to the DOM
+var newEl, newText, position;
+//Capture the position of insertion into the DOM
+position = document.getElementById('content');
+
+//Loop the array, extracting each array and writing information to the DOM
+//Note that the information is not 'clean'
+for(var i = 0; i < array.length; i++){
+	array[i] = calculateSTI(array[i]); //I think there is an error here. Should be calculateSTI(array[i])?
+ 	newEl = document.createElement('li');
+	newText = document.createTextNode(array[i]);
+	newEl.appendChild(newText);
+	position.appendChild(newEl);
+}
+
+function calculateSTI(array){
+  var newArray = [];
+
+  newArray[0] = array[0];
+
+  var employeeNumber = array[1];
+
+  var baseSalary = array[2];
+  var reviewScore = array[3];
+
+  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  if(bonus > 0.13){ //I think this is ok because the computation of bonus comes below this
+    bonus = 0.13;
+  }
+
+
+  newArray[1] = bonus;
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus)); //not rounded
+  newArray[3] = (baseSalary * bonus); 
+  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+
+
+  return newArray;
+}
+
+function getBaseSTI(reviewScore){
+  var basePercent;
+  switch(reviewScore){
+    case 1:
+      basePercent = 0;
+      break;
+    case 2:
+      basePercent = 0;
+      break;
+    case 3:
+      basePercent = 0.04;
+      break;
+    case 4:
+      basePercent = 0.06;
+      break;
+    case 5:
+      basePercent = 0.10;
+      break;
+  }
+  return basePercent; // changed basePercent -1 to simply basePercent because that it what we want from this function
+}
+
+function getYearAdjustment(employeeNumber){
+  var yearAdjustment = 0;
+  if(employeeNumber.length == 4){
+    yearAdjustment = 0.05;
+  }
+  return yearAdjustment;//I think this is ok
+}
+
+function getIncomeAdjustment(salary){
+  var incomeAdjustment = 0;
+  salary = parseInt(salary);
+  if(salary > 65000){
+    incomeAdjustment = 0.01;
+  }
+  return incomeAdjustment;//this looks ok if I want my incomeAdjustment positive which I checked and I do
+}
